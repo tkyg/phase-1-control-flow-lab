938d3208a4ad1b3d70dbe74fbcb6769977969d6f
tkyg <80114433+tkyg@users.noreply.github.com>
2021-10-21 16:18:59 -0400

    commit tree 417e1c525939b8d05d51e9c8b655f5a8560d0162

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){
+  if (someNumber <= 400){
+    return "This one is on me!"
+  } else if (someNumber > 2000 && someNumber < 2500){
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+  }
+  }
+  
+function ternaryCheckCity(funCity){
+  return funCity === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tipAmount){
+  switch (tipAmount){
+    case 'generous':
+      return 'Thank you so much.'
+      case 'not as generous':
+      return 'Thank you.'
+      default:
+        return 'Bye.'
+  }
+  
 }
